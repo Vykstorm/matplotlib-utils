cfe67cbe18bdc11a9b386b635ceb3eaf9ec2d3ce
Vykstorm <victorruizgomezdev@gmail.com>
2019-05-24 17:46:21 +0200

    Implemented method diffplot

--- plots.py
+++ plots.py
@@ -3,6 +3,7 @@
 from typing import *
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import collections as mc
 
 
 def filledfuncplot(x,
@@ -41,10 +42,39 @@ def filledfuncplot(x,
         plt.plot(x, y, color=color, alpha=borderalpha, linewidth=bordersize, label='_nolegend_')
 
 
+def diffplot(y, x=None,
+    marker=None, markersize=None, pointcolors=None, pointsalpha=None,
+    linewidths=1, linecolors=None, linestyles='-', linesalpha=None):
+
+    y = np.array(y)
+
+    if x is None:
+        x = np.arange(0, len(a))
+
+    if pointcolors is None and pointsalpha is None:
+        pointalpha = 1
+
+    if linecolors is None and linesalpha is None:
+        linesalpha = 0.5
+
+    if linecolors is None:
+        linecolors = 'black'
+
+    # Plot lines
+    lines = mc.LineCollection(
+        list(zip(np.stack([x, y[0]], axis=1), np.stack([x, y[1]], axis=1))),
+        linestyles=linestyles, linewidths=linewidths, colors=linecolors, alpha=linesalpha, label='_nolegend_')
+    plt.gca().add_collection(lines)
+
+    # Plot points
+    for k in (0, 1):
+        plt.scatter(x, y[k], marker=marker, s=markersize, c=pointcolors, alpha=pointsalpha)
+
 
 if __name__ == '__main__':
-    hist, edges = np.histogram(np.random.randn(1000), bins=30)
-
-    filledfuncplot(hist, color='blue', alpha=0.25, border=True, bordersize=1)
-    plt.legend(['f'])
+    a = np.random.random(10)
+    b = np.random.random(10)
+    diffplot((a, b), linewidths=1, pointcolors='blue', pointsalpha=1)
+    #plt.plot(a, b, None)
+    #plt.legend(['f'])
     plt.show()
